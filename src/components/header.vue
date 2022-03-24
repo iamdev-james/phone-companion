d6a2bf3edfc4cad7455cd39b288c1ff7062e5534
iamdev-james <jamesoluwabukola87@gmail.com>
2022-03-24 07:52:24 -0100

    Fixed some issues

--- src/components/header.vue
+++ src/components/header.vue
@@ -48,7 +48,7 @@
 					Get Started
 				</button>
 			</div>
-			<div class="container inline sm:hidden" @click="toggle()">
+			<div class="container inline md:hidden" @click="toggle()">
 				<div :class="{ move: scrollPosition > 1, change_1: navOpen }" class="bar1"></div>
 				<div :class="{ move: scrollPosition > 1, change_2: navOpen }" class="bar2"></div>
 				<div :class="{ move: scrollPosition > 1, change_3: navOpen }" class="bar3"></div>
