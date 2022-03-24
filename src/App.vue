3652494414812a77f8bedef0084df917a589c063
iamdev-james <jamesoluwabukola87@gmail.com>
2022-03-24 07:03:02 -0100

    Added smooth scroll

--- src/App.vue
+++ src/App.vue
@@ -406,6 +406,7 @@
 	import FAQ from "@/components/faq.vue";
 	import Footer from "@/components/footer.vue";
 	import Sidebar from "@/components/sidebar.vue";
+	import $ from "jquery";
 
 	export default {
 		name: "App",
@@ -418,6 +419,44 @@
 			Footer,
 			Sidebar,
 		},
+		mounted() {
+			// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus()
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus() // Set focus again
+          }
+        })
+      }
+    }
+  })
+		}
 	};
 </script>
 
